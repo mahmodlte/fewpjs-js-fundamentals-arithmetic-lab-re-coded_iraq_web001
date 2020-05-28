9c2841b705d8a93482f6807c3995d69832a25d60
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-28 15:16:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code 
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.random();
+const mod = 12 % 8;
+const max = Math.max(10,20);
