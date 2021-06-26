152d4bb8280743ddc406a031e42e1ae0055e22f9
Mahmoud Mansour <mahmodlte@gmail.com>
2021-06-26 10:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 const num1 = 2;
 const num2 = 31;
 
+<<<<<<< HEAD
 const multiply = num1 * num2;
 
 const random = Math.random();
 const mod = 12 % 8;
 const max = Math.max(10,20);
+=======
+const mulitply = num1 * num2;
+>>>>>>> 16e86a82fb775290500372ef3e4ff6e52b1f15f6
